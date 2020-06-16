6a40f4aa16d4f0d95e24e5e4384756ecd5ce2adb
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-06-16 16:22:00 +0200

    added laeckerliturm-ar

---
+++ Assets/scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(var t in Input.touches) {
+            if (t.phase != TouchPhase.Began)
+            continue;
+            var ray = Camera.main.ScreenPointToRay(t.position);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray, out hitInfo)) {
+                var go = GameObject.Instantiate(cubePrefab,hitInfo.point + new Vector3(0, 2, 0), Quaternion.identity);
+                // go.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+            }
+        }
+    }
+}

1aacd538f46fff711f311115d1bb4f9d078c36f5
David Maissen <david.maissen@students.fhnw.ch>
2020-07-07 21:58:03 +0200

    edit User Interface

--- Assets/scripts/main.cs
+++ Assets/scripts/main.cs
@@ -10,7 +10,7 @@ public class main : MonoBehaviour
     public void Explore()
     {
 
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("explore");
 
     }
 

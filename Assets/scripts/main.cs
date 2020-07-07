2d62dcc177547a8dc85e24c1d3efcd4515383630
David Maissen <david.maissen@students.fhnw.ch>
2020-07-07 16:24:18 +0200

    Implement User Interface

---
+++ Assets/scripts/main.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+
+    public void Explore()
+    {
+
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void ShowTutorial()
+    {
+
+        SceneManager.LoadScene("tutorial");
+
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
+
+
+    public void OpenPanel()
+    {
+        if(SettingsPanel != null){
+            SettingsPanel.SetActive(true);
+        }
+    }
+}

4ebcf72f88caccd5cd8a64de62e48d485047f971
David Maissen <david.maissen@students.fhnw.ch>
2020-07-17 00:15:40 +0200

    implement Menu Animation

---
+++ Assets/scripts/ExploreController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ExploreController : MonoBehaviour
+{
+    public TextMeshProUGUI starsCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TextMeshPro textmeshPro = starsCounter.GetComponent<TextMeshPro>();
+        //starsCounter.SetText();
+
+        starsCounter = starsCounter.GetComponent<TextMeshProUGUI>();
+        starsCounter.text = GameProgress.starsCollected.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

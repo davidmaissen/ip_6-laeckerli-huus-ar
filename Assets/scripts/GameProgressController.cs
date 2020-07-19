367b8f5cb46af3cded8be02f757079dcdc1b1dee
David Maissen <david.maissen@students.fhnw.ch>
2020-07-19 10:15:59 +0200

    Edit Explore UI

---
+++ Assets/scripts/GameProgressController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameProgressController : MonoBehaviour
+{
+    public TextMeshProUGUI starsCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateStarsCounter(starsCounter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void updateStarsCounter(TextMeshProUGUI starsCounter)
+    {
+        //set value of starsCounter
+        Debug.Log(GameProgress.starsCollected.ToString());
+        starsCounter = starsCounter.GetComponent<TextMeshProUGUI>();
+        starsCounter.text = GameProgress.starsCollected.ToString();
+
+    }
+}

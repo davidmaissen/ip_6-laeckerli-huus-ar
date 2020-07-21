efde94f66a502fefa0a93e269560bb98e2a9bb19
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-07-21 14:52:01 +0200

    explore: star-catching is implemented (gameprogress updating is not updatet)

--- Assets/scripts/MarkerTracking.cs
+++ Assets/scripts/MarkerTracking.cs
@@ -15,6 +15,9 @@ public class MarkerTracking : MonoBehaviour
 
     private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
     private ARTrackedImageManager trackedImageManager;
+    private bool airPlaneStarCollected = false;
+    private bool oldImagesStarCollected = false;
+    private bool museStarCollected = false;
 
     private void Awake() {
         trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
@@ -49,6 +52,8 @@ public class MarkerTracking : MonoBehaviour
     }
 
     private void UpdateImage(ARTrackedImage trackedImage) {
+        Debug.Log("Is star collected: " + IsStarCollected(trackedImage.referenceImage.name));
+        if (IsStarCollected(trackedImage.referenceImage.name)) return;
         string name = trackedImage.referenceImage.name;
         Vector3 position = trackedImage.transform.position;
 
@@ -77,10 +82,18 @@ public class MarkerTracking : MonoBehaviour
             if(Physics.Raycast(ray, out hitInfo)) {
                 Debug.Log(PositionSaveSystem.rotation + " -- " + PositionSaveSystem.position);
                 Debug.Log(hitInfo.transform.gameObject.name + " clicked");
+                /*
                 if (hitInfo.transform.gameObject.name == "find-alex") {
                     StartCoroutine(LoadAsyncScene(hitInfo.transform.gameObject.name));
                 }
-                SceneManager.LoadScene(hitInfo.transform.gameObject.name);
+                */
+                if (hitInfo.transform.gameObject.name == "airplane-star" ||
+                    hitInfo.transform.gameObject.name == "old-images-star" || 
+                    hitInfo.transform.gameObject.name == "muse-star") {
+                    StarCollected(hitInfo.transform.gameObject.name);
+                } else {
+                    StartCoroutine(LoadAsyncScene(hitInfo.transform.gameObject.name));
+                }
                 /*
              if (
                  hitInfo.transform.gameObject.name == "laeckerli-tower" ||
@@ -90,6 +103,23 @@ public class MarkerTracking : MonoBehaviour
              */
             }
         }
+    }
+
+    private void StarCollected(string name) {
+        if (name == "airplane-star") airPlaneStarCollected = true;
+        else if (name == "old-images-star") oldImagesStarCollected = true;
+        else if (name == "muse-star") museStarCollected = true;
+        foreach(GameObject go in spawnedPrefabs.Values) {
+            if (go.name == name) {
+                go.SetActive(false);
+                Debug.Log(go.name + " is active: " + go.activeSelf);
+            }
+        }
+    }
+    private bool IsStarCollected(string name) {
+        return !name.Contains("-star") || (name == "airplane-star" && airPlaneStarCollected) || 
+        (name == "old-images-star" && oldImagesStarCollected) ||
+        (name == "muse-star" && museStarCollected);
     }
 
     IEnumerator LoadAsyncScene(string name)

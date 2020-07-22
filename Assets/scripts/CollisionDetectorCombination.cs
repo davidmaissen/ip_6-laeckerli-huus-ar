1452c4b76c3e9bd70e03490bf72ead03c8083a5a
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-07-22 21:10:12 +0200

    combination: collisions can be detected on each side

---
+++ Assets/scripts/CollisionDetectorCombination.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetectorCombination : MonoBehaviour
+{
+    private void Update() {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo)) {
+            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+                Debug.Log("Collision without Rigidbody down " + this.name + " with: " + 
+                hitInfo.transform.gameObject.name);
+            }
+        }
+        if (Physics.Raycast(transform.position, Vector3.up, out hitInfo)) {
+            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+                Debug.Log("Collision without Rigidbody up " + this.name + " with: " + 
+                hitInfo.transform.gameObject.name);
+            }
+        }
+        if (Physics.Raycast(transform.position, Vector3.right, out hitInfo)) {
+            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+                Debug.Log("Collision without Rigidbody right " + this.name + " with: " + 
+                hitInfo.transform.gameObject.name);
+            }
+        }
+        if (Physics.Raycast(transform.position, Vector3.left, out hitInfo)) {
+            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+                Debug.Log("Collision without Rigidbody left " + this.name + " with: " + 
+                hitInfo.transform.gameObject.name);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/ImageTrackingCombination.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.SceneManagement;
+
+// Source: https://www.youtube.com/watch?v=I9j3MD7gS5Y
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImageTrackingCombination : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] placeablePrefab;
+
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+    private ARTrackedImageManager trackedImageManager;
+    private StarsCountController starsCountController;
+    private static bool airPlaneStarCollected = false;
+    private static bool oldImagesStarCollected = false;
+    private static bool museStarCollected = false;
+
+    private void Awake() {
+        trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+        starsCountController = FindObjectOfType<StarsCountController>();
+
+        foreach(GameObject prefab in placeablePrefab)
+        {
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            newPrefab.name = prefab.name;
+            newPrefab.SetActive(false);
+            spawnedPrefabs.Add(prefab.name, newPrefab);
+        }
+    }
+
+    private void OnEnable() {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
+
+    private void OnDisable() {
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
+    }
+
+    private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs) {
+        foreach(ARTrackedImage trackedImage in eventArgs.added) {
+            UpdateImage(trackedImage);
+        }
+        foreach(ARTrackedImage trackedImage in eventArgs.updated) {
+            UpdateImage(trackedImage);
+        }
+        foreach(ARTrackedImage trackedImage in eventArgs.removed) {
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage) {
+        string name = trackedImage.referenceImage.name;
+        Vector3 position = trackedImage.transform.position;
+
+        GameObject prefab = spawnedPrefabs[name];
+        prefab.transform.position = position;
+        prefab.transform.rotation = trackedImage.transform.rotation;
+        PositionSaveSystem.position = prefab.transform.position;
+        PositionSaveSystem.rotation = prefab.transform.rotation;
+        prefab.transform.Rotate(90,0,0);
+        prefab.SetActive(true);
+        Debug.Log(prefab.name + " spotted");
+
+        /*
+        foreach(GameObject go in spawnedPrefabs.Values) {
+            if (go.name != name) {
+                go.SetActive(false);
+            }
+        }
+        */
+    }
+
+     void Update(){
+     }
+}

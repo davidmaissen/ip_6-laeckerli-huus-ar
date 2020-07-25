6d09c7c1a01a63d4fc06a0b65a6214a5c9eb2973
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-07-25 16:20:10 +0200

    combine - created first game to combine 4 pieces of a bowl

--- Assets/scripts/CollisionDetectorCombination.cs
+++ Assets/scripts/CollisionDetectorCombination.cs
@@ -5,40 +5,77 @@ using UnityEngine;
 public class CollisionDetectorCombination : MonoBehaviour
 {
     public GameObject collision;
+    private CombinationController combinationController;
 
     private void Awake() {
-        
+        combinationController = GameObject.FindObjectOfType<CombinationController>();
     }
+    
     private void Update() {
         RaycastHit hitInfo;
-        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo)) {
-            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+        if (Physics.Raycast(transform.position, -transform.forward, out hitInfo)) {
+            if (!(this.name == hitInfo.transform.gameObject.name) && hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
                 Debug.Log("Collision without Rigidbody down " + this.name + " with: " + 
                 hitInfo.transform.gameObject.name);
                 Instantiate(collision, hitInfo.transform.position + new Vector3(0,this.GetComponent<Collider>().bounds.size.x/2,0), hitInfo.transform.rotation);
+                
+                if (this.name == "bowl-topright" && hitInfo.transform.gameObject.name == "bowl-bottomright") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(1, this.name);
+                }
+                if (this.name == "bowl-topleft" && hitInfo.transform.gameObject.name == "bowl-bottomleft") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(3, this.name);
+                }
             }
         }
-        if (Physics.Raycast(transform.position, Vector3.up, out hitInfo)) {
-            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo)) {
+            if (!(this.name == hitInfo.transform.gameObject.name) && hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
                 Debug.Log("Collision without Rigidbody up " + this.name + " with: " + 
                 hitInfo.transform.gameObject.name);
                 Instantiate(collision, hitInfo.transform.position - new Vector3(0,this.GetComponent<Collider>().bounds.size.x/2,0), hitInfo.transform.rotation);
+                if (this.name == "bowl-bottomright" && hitInfo.transform.gameObject.name == "bowl-topright") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(1, this.name);
+                }
+                if (this.name == "bowl-bottomleft" && hitInfo.transform.gameObject.name == "bowl-topleft") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(3, this.name);
+                }
             }
         }
-        if (Physics.Raycast(transform.position, Vector3.right, out hitInfo)) {
-            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+        if (Physics.Raycast(transform.position, transform.right, out hitInfo)) {
+            if (!(this.name == hitInfo.transform.gameObject.name) && hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
                 Debug.Log("Collision without Rigidbody right " + this.name + " with: " + 
                 hitInfo.transform.gameObject.name);
                 GameObject go = Instantiate(collision, hitInfo.transform.position - new Vector3(this.GetComponent<Collider>().bounds.size.x/2,0,0), hitInfo.transform.rotation);
                 go.transform.Rotate(0,0,90);
+
+                if (this.name == "bowl-topleft" && hitInfo.transform.gameObject.name == "bowl-topright") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(0, this.name);
+                }
+                if (this.name == "bowl-bottomleft" && hitInfo.transform.gameObject.name == "bowl-bottomright") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(2, this.name);
+                }
             }
         }
-        if (Physics.Raycast(transform.position, Vector3.left, out hitInfo)) {
-            if (hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
+        if (Physics.Raycast(transform.position, -transform.right, out hitInfo)) {
+            if (!(this.name == hitInfo.transform.gameObject.name) && hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
                 Debug.Log("Collision without Rigidbody left " + this.name + " with: " + 
                 hitInfo.transform.gameObject.name);
                 GameObject go = Instantiate(collision, hitInfo.transform.position + new Vector3(this.GetComponent<Collider>().bounds.size.x/2,0,0), hitInfo.transform.rotation);
-                go.transform.Rotate(0,0,90);            }
+                go.transform.Rotate(0,0,90);
+                if (this.name == "bowl-bottomright" && hitInfo.transform.gameObject.name == "bowl-bottomleft") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(2, this.name);
+                }
+                if (this.name == "bowl-topright" && hitInfo.transform.gameObject.name == "bowl-topleft") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(0, this.name);
+                }
+            }
         }
     }
 }
---
+++ Assets/scripts/CombinationController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public class CombinationController : MonoBehaviour
+{
+    private bool[] bowlAddedCorrectly;
+    private bool[] spoonAddedCorrectly;
+    private bool[] tableAddedCorrectly;
+    public Canvas canvas;
+
+    private void Awake() {
+        bowlAddedCorrectly = new bool[4];
+        spoonAddedCorrectly = new bool[3];
+        tableAddedCorrectly = new bool[3];
+    }
+
+    private void Update() {
+        /*
+        Debug.Log("---");
+        foreach (bool b in bowlAddedCorrectly) Debug.Log(b);
+        Debug.Log("---");
+        */
+        if (Array.TrueForAll(bowlAddedCorrectly, value => { return value; })) {
+            Debug.Log("bowlAddedCorrectly");
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void UpdateCombination(int index, string name) {
+        StartCoroutine(CollisionUpdate(index, name));
+    }
+
+    IEnumerator CollisionUpdate(int index, string name)
+    {
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        if (name.Contains("bowl") && !bowlAddedCorrectly[index]) {
+            bowlAddedCorrectly[index] = true;
+            while (watch.Elapsed.TotalSeconds < 0.5) {
+                yield return null;
+            }
+            bowlAddedCorrectly[index] = false;
+        } else if (name.Contains("spoon") && !spoonAddedCorrectly[index]) {
+            spoonAddedCorrectly[index] = true;
+            while (watch.Elapsed.TotalSeconds < 0.5) {
+                yield return null;
+            }
+            spoonAddedCorrectly[index] = false;
+        } else if (name.Contains("table") && !tableAddedCorrectly[index]) {
+            tableAddedCorrectly[index] = true;
+            while (watch.Elapsed.TotalSeconds < 0.5) {
+                yield return null;
+            }
+            tableAddedCorrectly[index] = false;
+        }
+        StopCoroutine("CollisionUpdate");       
+    }
+}

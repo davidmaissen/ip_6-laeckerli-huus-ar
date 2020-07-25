5ec49c2929fecb9f900f9834e92fbb418329ffb5
David Maissen <david.maissen@students.fhnw.ch>
2020-07-25 11:36:08 +0200

    Fix visibilty tutorial-panel

---
+++ Assets/scripts/CanvasStateController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasStateController : MonoBehaviour
+{
+
+    public GameObject tutorial;
+    private GameProgress gameProgress;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameProgress = new GameProgress();
+        gameProgress.InitializeGameData();
+
+        //set canvas visibilty
+        tutorial.SetActive(false);
+
+
+        if(!(GameProgress.tutorialCompleted))
+        {
+            showTutorial(tutorial);
+            GameProgress.tutorialCompleted = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+        public void showTutorial(GameObject tutorial)
+    {
+        tutorial.SetActive(true);
+    }
+
+}

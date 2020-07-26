caf452ba367667d9a67b47afa88a0185ecdc3062
Fabian Bur <fabian.bur@students.fhnw.ch>
2020-07-26 17:23:42 +0200

    combine: added whisk and created logic for detection. not yet tested with image tracking

--- Assets/scripts/CollisionDetectorCombination.cs
+++ Assets/scripts/CollisionDetectorCombination.cs
@@ -12,6 +12,14 @@ public class CollisionDetectorCombination : MonoBehaviour
     }
     
     private void Update() {
+        if (this.name.Contains("bowl")) {
+            BowlCollision();
+        } else if (this.name.Contains("whisk")) {
+            WhiskCollision();
+        }
+    }
+
+    private void BowlCollision() {
         RaycastHit hitInfo;
         if (Physics.Raycast(transform.position, -transform.forward, out hitInfo)) {
             if (!(this.name == hitInfo.transform.gameObject.name) && hitInfo.distance - this.GetComponent<Collider>().bounds.size.x/2 < 0) {
@@ -49,7 +57,7 @@ public class CollisionDetectorCombination : MonoBehaviour
                 Debug.Log("Collision without Rigidbody right " + this.name + " with: " + 
                 hitInfo.transform.gameObject.name);
                 GameObject go = Instantiate(collision, hitInfo.point, hitInfo.transform.rotation);
-                go.transform.Rotate(0,0,180);
+                go.transform.Rotate(0,0,90);
 
                 if (this.name == "bowl-topleft" && hitInfo.transform.gameObject.name == "bowl-topright") {
                     Debug.Log(this.name + " is placed right");
@@ -66,7 +74,7 @@ public class CollisionDetectorCombination : MonoBehaviour
                 Debug.Log("Collision without Rigidbody left " + this.name + " with: " + 
                 hitInfo.transform.gameObject.name);
                 GameObject go = Instantiate(collision, hitInfo.point, hitInfo.transform.rotation);
-                go.transform.Rotate(0,0,180);
+                go.transform.Rotate(0,0,90);
                 if (this.name == "bowl-bottomright" && hitInfo.transform.gameObject.name == "bowl-bottomleft") {
                     Debug.Log(this.name + " is placed right");
                     combinationController.UpdateCombination(2, this.name);
@@ -77,5 +85,23 @@ public class CollisionDetectorCombination : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void WhiskCollision() {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, -transform.right, out hitInfo)) {
+            if (!(this.name == hitInfo.transform.gameObject.name) && hitInfo.distance - this.GetComponent<Collider>().bounds.size.z/2 < 0) {
+                Debug.Log("Collision without Rigidbody left " + this.name + " with: " + 
+                hitInfo.transform.gameObject.name);
+                GameObject go = Instantiate(collision, hitInfo.point, hitInfo.transform.rotation);
+                go.transform.Rotate(0,0,90);
+                if (this.name == "whisk-handle" && hitInfo.transform.gameObject.name == "whisk-wires"
+                || this.name == "whisk-wires" && hitInfo.transform.gameObject.name == "whisk-handle") {
+                    Debug.Log(this.name + " is placed right");
+                    combinationController.UpdateCombination(0, this.name);
+                }
+            }
+        }
+
     }
 }

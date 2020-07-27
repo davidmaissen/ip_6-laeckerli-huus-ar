d2fc74e0c1a08e887a91909b0b5a6aa5c5e369e3
David Maissen <david.maissen@students.fhnw.ch>
2020-07-27 20:40:32 +0200

    implement responsive panels in explore

--- Assets/scripts/AnimationController.cs
+++ Assets/scripts/AnimationController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+
+    private OrientationLayoutController layoutController;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +21,36 @@ public class AnimationController : MonoBehaviour
     public void animateIngredientsBar(Animator animation)
     {
         animation.SetBool("expanded", !animation.GetBool("expanded"));
-
     }
 
-    public void ToggleVisible(Animator animation)
+     public void ToggleVisible(Animator animation)
     {
         //var anim = GetComponent<Animator>();
         animation.SetBool("displayed", !animation.GetBool("displayed"));
-    }
+    } 
+
+
+     public void TogglePanel(GameObject panel)
+    {
+        layoutController = GetComponent<OrientationLayoutController>();
+
+        if(layoutController.IsPortrait())
+        {
+            Animator animatorPortrait = panel.transform.GetChild(0).transform.GetComponent<Animator>();      
+            animatorPortrait.SetBool("displayed", !animatorPortrait.GetBool("displayed"));
+  
+        }
+        else
+        {
+            Debug.Log("Button pressed");
+            Animator animatorLandscape = panel.transform.GetChild(1).transform.GetComponent<Animator>();
+            Debug.Log(panel.transform.GetChild(1).name);
+            animatorLandscape.SetBool("displayed", !animatorLandscape.GetBool("displayed"));
+            
+        }
+        //var anim = GetComponent<Animator>();
+      //  animation.SetBool("displayed", !animation.GetBool("displayed"));
+    } 
+
 
 }
--- Assets/scripts/ButtonStateController.cs
+++ Assets/scripts/ButtonStateController.cs
@@ -7,7 +7,10 @@ public class ButtonStateController : MonoBehaviour
 {
     public Sprite imageInactive;
     public Sprite imageActive;
-    public GameObject[] buttons;
+
+    public bool isButtonGroup;
+  //  public GameObject[] ingredientButtons;
+    //public GameObject[] languageButtons;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +24,59 @@ public class ButtonStateController : MonoBehaviour
 
     }
 
-    public void changeImage(GameObject buttonClicked)
+    public void setButtonStates()
+    {
+        GameObject buttonClicked = this.gameObject;
+
+
+        if (buttonClicked.transform.GetComponent<Image>().sprite == imageActive)
+        {
+            buttonClicked.transform.GetComponent<Image>().sprite = imageInactive;
+        }
+        else
+        {
+            buttonClicked.transform.GetComponent<Image>().sprite = imageActive;
+        }
+
+        if(isButtonGroup)
+        {
+            GameObject buttonGroup = buttonClicked.transform.parent.gameObject;
+            for (int i = 0; i < buttonGroup.transform.childCount; i++)
+            {
+                GameObject button =  buttonGroup.transform.GetChild(i).gameObject;
+                if(button != buttonClicked)
+                 {
+                button.transform.GetComponent<Image>().sprite = imageInactive;
+                }
+            }
+
+        }
+    }
+}
+
+        
+      //  for (int i = 0; i < gameObject.transform.childCount; i++)
+       // {
+        //}
+     /*        GameObject buttonGroup = this.gameObject;
+            GameObject button =  buttonGroup.transform.GetChild(i).gameObject;
+            if(button == buttonClicked)
+            {
+                if (button.transform.GetComponent<Image>().sprite == imageActive)
+                {
+                    button.transform.GetComponent<Image>().sprite = imageInactive;
+                }
+                else
+                {
+                    button.transform.GetComponent<Image>().sprite = imageInactive;
+                }
+
+            } */
+            
+            //gameObject.transform.GetChild(i).transform.GetComponent<Image>().sprite = imageActive;
+
+
+/*     public void changeImage(GameObject buttonClicked)
     {
         foreach(GameObject button in buttons)
         {
@@ -44,5 +99,4 @@ public class ButtonStateController : MonoBehaviour
 
 
 
-    }
-}
+    } */
